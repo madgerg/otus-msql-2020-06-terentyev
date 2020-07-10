594400bb786e6aab3ddbdf09582176b8ee3fc493
madgerg <45966270+madgerg@users.noreply.github.com>
2020-07-10 09:28:09 +0500

    Update SQLQuery4.sql

--- HW02/SQLQuery4.sql
+++ HW02/SQLQuery4.sql
@@ -1,10 +1,10 @@
---4. Заказы поставщикам (Purchasing.Suppliers), которые были исполнены в январе 2014 года с доставкой Air Freight или Refrigerated Air Freight (DeliveryMethodName).
---Вывести:
---* способ доставки (DeliveryMethodName)
---* дата доставки
---* имя поставщика
---* имя контактного лица принимавшего заказ (ContactPerson)
---Таблицы: Purchasing.Suppliers, Purchasing.PurchaseOrders, Application.DeliveryMethods, Application.People.
+--4. Р—Р°РєР°Р·С‹ РїРѕСЃС‚Р°РІС‰РёРєР°Рј (Purchasing.Suppliers), РєРѕС‚РѕСЂС‹Рµ Р±С‹Р»Рё РёСЃРїРѕР»РЅРµРЅС‹ РІ СЏРЅРІР°СЂРµ 2014 РіРѕРґР° СЃ РґРѕСЃС‚Р°РІРєРѕР№ Air Freight РёР»Рё Refrigerated Air Freight (DeliveryMethodName).
+--Р’С‹РІРµСЃС‚Рё:
+--* СЃРїРѕСЃРѕР± РґРѕСЃС‚Р°РІРєРё (DeliveryMethodName)
+--* РґР°С‚Р° РґРѕСЃС‚Р°РІРєРё
+--* РёРјСЏ РїРѕСЃС‚Р°РІС‰РёРєР°
+--* РёРјСЏ РєРѕРЅС‚Р°РєС‚РЅРѕРіРѕ Р»РёС†Р° РїСЂРёРЅРёРјР°РІС€РµРіРѕ Р·Р°РєР°Р· (ContactPerson)
+--РўР°Р±Р»РёС†С‹: Purchasing.Suppliers, Purchasing.PurchaseOrders, Application.DeliveryMethods, Application.People.
 
 use WideWorldImporters
 select   
@@ -22,6 +22,6 @@ select
 where 
 dm.DeliveryMethodName like '%Air Freight' and 
 --dm.DeliveryMethodName = 'Air Freight' or dm.DeliveryMethodName = 'Refrigerated Air Freight' and
---можно было бы сделать выборку (dm.DeliveryMethodName = 'Air Freight' or dm.DeliveryMethodName = 'Refrigerated Air Freight' ), но при текущем варианте так же ищет по стоимости запроса в плане
+--РјРѕР¶РЅРѕ Р±С‹Р»Рѕ Р±С‹ СЃРґРµР»Р°С‚СЊ РІС‹Р±РѕСЂРєСѓ (dm.DeliveryMethodName = 'Air Freight' or dm.DeliveryMethodName = 'Refrigerated Air Freight' ), РЅРѕ РїСЂРё С‚РµРєСѓС‰РµРј РІР°СЂРёР°РЅС‚Рµ С‚Р°Рє Р¶Рµ РёС‰РµС‚ РїРѕ СЃС‚РѕРёРјРѕСЃС‚Рё Р·Р°РїСЂРѕСЃР° РІ РїР»Р°РЅРµ
 po.ExpectedDeliveryDate between '2014-01-01' and '2014-01-31'
---не выдаст ничего, т.к. с такими параметрами нет записей (либо в 2013 году искать, либо другие методы доставки использовать)
+--РЅРµ РІС‹РґР°СЃС‚ РЅРёС‡РµРіРѕ, С‚.Рє. СЃ С‚Р°РєРёРјРё РїР°СЂР°РјРµС‚СЂР°РјРё РЅРµС‚ Р·Р°РїРёСЃРµР№ (Р»РёР±Рѕ РІ 2013 РіРѕРґСѓ РёСЃРєР°С‚СЊ, Р»РёР±Рѕ РґСЂСѓРіРёРµ РјРµС‚РѕРґС‹ РґРѕСЃС‚Р°РІРєРё РёСЃРїРѕР»СЊР·РѕРІР°С‚СЊ)
